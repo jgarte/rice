5ab47c7ca0cb14e9afe5bfd21408523934d7ec30
depsterr <dragonvale09@gmail.com>
2020-05-16 23:08:32 +0200

    fixed autostart script

--- configs/.config/dwm/autostart.sh
+++ configs/.config/dwm/autostart.sh
@@ -17,7 +17,7 @@ xrandr --output DP2 --mode 1280x1024 --left-of eDP1
 
 # bar
 # for whatever reason < /sys/class/power_supply/BAT1/capacity doesn't work, so I have to use cat :/ (works in bash and zsh, but not in dash)
-if [ -d "/sys/class/power_supply/" ]; then
+if [ -d "/sys/class/power_supply/BAT1" ]; then
 	while true; do
 		ADDR=`ip addr | awk '!/127.0.0.1/&&/inet / { gsub("/"," "); print $2; }' | sed '1q'`
 		xsetroot -name " $([ -z "$ADDR" ] && echo "No Internet" || echo "$ADDR") | $(cat /sys/class/power_supply/BAT1/capacity)% | $(cat /sys/class/power_supply/BAT1/status) | $(date +%a\ %d\ %b\ %R:%S) "
