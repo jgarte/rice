f716afa883cd2d648f50e0b52ed32f5fc95fd48a
depsterr <depsterr@protonmail.com>
2020-05-18 15:01:29 +0200

    updated statusbar

--- configs/.config/dwm/status.sh
+++ configs/.config/dwm/status.sh
@@ -1,16 +1,31 @@
-#! /bin/sh
+#! /bin/bash
 
 # Output before ; is to the right, after is to the left
 
+BATDIR=`find /sys/class/power_supply -name 'BAT*' | sed '1q'`
+
 while true; do
-	DATESTR=`date +%a\ %d\ %b\ %R:%S`
+	DATESTR="📅 `date +%a\ %d\ %b\ %R:%S`"
+
+
+	BATSTR=""
+	[ -z "${BATDIR}" ] || {
+		BATSTR="| 🔋 `< ${BATDIR}/capacity`% `< ${BATDIR}/status` | "
+	}
+
+	ADDRSTR="📶"
 	ADDRS=`ip addr |\
-	   	awk '!/127.0.0.1/&&/inet / { gsub("/"," "); print $2; }'`
-	[ -z "$ADDRS" ] && ADDRS="No Internet"
-	ADDRSTR=""
+	   	awk '/inet / { gsub("/"," "); print $2; }'`
+	[ -z "${ADDRS}" ] && ADDRS="No Internet"
 	for ADDR in $ADDRS; do
-		ADDRSTR+="${ADDR} | "
+		ADDRSTR+=" ${ADDR}"
 	done
-	xsetroot -name "${DATE};${ADDRSTR}"
+
+	FREESTR="💾 `free -h | awk '/Mem:/ { print $3 }'` / `free -h | awk '/Mem:/ { print $2 }'`"
+
+	DISKSTR="📁 `df -H | awk '/ \/$/ { print $3 }'` / `df -H | awk '/ \/$/ { print $2 }'`"
+
+	# BATSTR is ugly to make it work if no battery is connected
+	xsetroot -name "${BATSTR}${DATESTR}; ${ADDRSTR} | ${FREESTR} | ${DISKSTR}"
 	sleep 5
 done
