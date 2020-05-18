e2d27f67dc64e9fae13d59c4147a2189d15e6d9e
depsterr <depsterr@protonmail.com>
2020-05-18 02:01:07 +0200

    removed stuff not different from master

--- configs/.zprofile
+++
@@ -1,4 +0,0 @@
-export PATH=$PATH:/sbin/:/snap/bin/:~/.scripts/:~/.gem/ruby/2.6.0/gems:/opt/
-if [ "$(tty)" = "/dev/tty1" ]; then
-	sh ~/.scripts/login_script
-fi
--- installer.sh
+++ installer.sh
@@ -11,9 +11,3 @@ echo "===> Installing st"
 cd terminal/st
 sudo make install
 cd $SRCDIR
-
-echo "===> Installing configuration files"
-cp configs/. ~ -Ra
-
-echo "===> Installing scripts"
-cp scripts ~/.scripts -r
