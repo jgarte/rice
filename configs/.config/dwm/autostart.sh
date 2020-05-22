84cbeb76902b63fbaee4aaa38584c94e3df7b3fa
depsterr <depsterr@protonmail.com>
2020-05-22 05:08:09 +0200

    added redsift to autostart

--- configs/.config/dwm/autostart.sh
+++ configs/.config/dwm/autostart.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
+# status bar
+bash ~/.config/dwm/status.sh &
+
 # xclip
 xclip &
 
 # picom
 picom &
 
-# resolution of second monitor
-xrandr --output DP2 --mode 1280x1024 --left-of eDP1
-
 # wallpaper
 ~/.fehbg &
 
-bash ~/.config/dwm/status.sh &
+# redshift
+redshift &
